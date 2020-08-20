0bd85413ecee6d2772a745d532967c00377f8318
gustavo-silvalara <gustavo.silvalara@outlook.com>
2020-08-19 22:34:17 -0400

    commit proj init

---
+++ src/main/java/com/github/gustavolara/ifmt/cobrancaifmt/CobrancaifmtApplication.java
@@ -0,0 +1,13 @@
+package com.github.gustavolara.ifmt.cobrancaifmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CobrancaifmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CobrancaifmtApplication.class, args);
+	}
+
+}
